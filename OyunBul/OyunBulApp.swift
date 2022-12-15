1fedcd8be138e08411abc1a5d0283b61a32f71ec
Hüseyin Savaş <savas.08@hotmail.com>
2022-12-15 22:58:46 +0300

    Initial Commit

---
+++ OyunBul/OyunBulApp.swift
@@ -0,0 +1,17 @@
+//
+//  OyunBulApp.swift
+//  OyunBul
+//
+//  Created by Hüseyin Savaş on 15.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct OyunBulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
